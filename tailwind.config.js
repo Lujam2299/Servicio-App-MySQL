be7737dab29248ef0a759988b3ec2a19cda47111
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-01-04 23:47:37 -0600

    Add Tailwind CSS and update home route

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.blade.php", "./src/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
