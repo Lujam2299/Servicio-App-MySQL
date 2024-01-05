1700ae480172640220d53ecbe0776a7e6f9afad3
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-01-05 01:11:54 -0600

    Refactor authentication controller and user migration
    This commit refactors the authentication controller by adding a new method for storing user credentials. It also updates the user migration to make the 'name' field nullable. These changes improve the authentication process and allow for more flexibility in user data.

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers\auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
   public function index()
   {
     return view('auth.students');
+  }
+
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'email' => 'required|email',
+      'password' => 'required'
+    ]);
+
+    if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+      return back()->with('status', 'Credenciales no válidas');
+    }
+
+    return redirect()->route('home');
   }
 
   public function admin()
