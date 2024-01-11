cc5e8ebbbff26ca6dd4481df1475c9ab2ed5847b
Lujam2299 <21690001@tecvalles.mx>
2024-01-10 20:27:29 -0600

    Vistas de Pestañas, Inicio, configuración y Solicitudes, así como su respectiva funcionalidad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,21 @@
 @extends('layout.layout')
 
+
 @section("content")
+<h2 class="text-black "><b>SISTEMA DE GESTION DE ESTANCIAS</b></h2><br>
 <div class="flex justify-center gap-3">
   <a href="{{route("auth.students")}}" class="bg-blue-700 text-white hover:bg-blue-800 cursor-pointer rounded-lg p-2 text-xl">Alumno - CESA</a>
-  <a class="text-black border border-blue-600 cursor-pointer rounded-lg p-2 text-xl">Docente</a>
+  <a href="{{route("auth.docente")}}" class="text-black border border-blue-600 cursor-pointer rounded-lg p-2 text-xl">Docente</a>
   <a href="{{route("auth.admin")}}" class="text-black border border-blue-600 cursor-pointer rounded-lg p-2 text-xl">Administrador</a>
 </div>
 <form
   class="mt-8 border p-3"
-  
   >
   @csrf
   <label class="block text-lg font-medium text-blue-700" for="controlNumber">
     Número de control
   </label>
-  <Input class="mt-2 mb-4 border-blue-300 p-2 w-full rounded-md" id="controlNumber" placeholder="19690395" type="number" min="1" max="8"/>
+  <Input class="mt-2 mb-4 border-blue-300 p-2 w-full rounded-md" id="controlNumber" placeholder="19690001" type="number" min="1" max="8"/>
   <label class="block text-lg font-medium text-blue-700" for="password">
     Contraseña
   </label>
---
+++ resources/views/layout/layout-admin-dashboard.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Servicio - App</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <div class="flex flex-col h-screen bg-gray-100 dark:bg-gray-900">
+    <header class="flex items-center justify-between px-6 py-4 bg-white shadow-md dark:bg-gray-800">
+      <div class="text-2xl font-semibold text-gray-700 dark:text-gray-200">Panel de Administración</div>
+      <div class="flex items-center space-x-3">
+        <span class="text-gray-700 dark:text-gray-200">Hola, Usuario</span>
+        <Button class="text-sm" variant="outline">
+          Cerrar Sesión
+        </Button>
+      </div>
+    </header>
+    <main class="flex flex-1 overflow-auto">
+      <nav class="flex flex-col bg-white shadow-lg dark:bg-gray-800 w-64">
+        <div class="flex items-center justify-center mt-8">
+          <div class="flex items-center">
+            <span class="text-gray-600 dark:text-gray-400 text-2xl font-semibold">Tablero</span>
+          </div>
+        </div>
+        <nav class="mt-10 px-6 ">
+          <a
+            class="mt-3 flex items-center py-2 px-4 text-gray-700 rounded-md dark:bg-gray-700 dark:text-gray-200"
+            href="#"
+          >
+            
+            <span class="mx-4 font-medium">Inicio</span>
+          </a>
+          <a
+            href="{{route('admin.create-user')}}"
+            class="mt-3 flex items-center py-2 px-4 text-gray-600 rounded-md dark:text-gray-400" href="#">
+            
+            <span class="mx-4 font-medium">Usuarios</span>
+          </a>
+          <a class="mt-3 flex items-center py-2 px-4 text-gray-600 rounded-md dark:text-gray-400" href="#">
+            
+            <span class="mx-4 font-medium">Configuraciones</span>
+          </a>
+          <a class="mt-3 flex items-center py-2 px-4 text-gray-600 rounded-md dark:text-gray-400" href="#">
+            
+            <span class="mx-4 font-medium">Sala de Conferencias</span>
+          </a>
+          <a class="mt-3 flex items-center py-2 px-4 text-gray-600 rounded-md dark:text-gray-400" href="#">
+            
+            <span class="mx-4 font-medium">Solicitudes</span>
+          </a>
+        </nav>
+      </nav>
+      <div class="flex flex-col flex-1 p-6 overflow-hidden">
+        @yield('content-admin-dashboard')
+      </div>
+    </main>
+  </div>
+</body>
+</html>
